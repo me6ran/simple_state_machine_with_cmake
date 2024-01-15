3d0d6140a740ee44cd03dac22db6d8988f1d73ec
Mason B <hatef.ir@gmail.com>
2024-01-15 10:21:31 -0800

    Enable users to specify target name to build

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 BUILD_DIR=build
 mkdir -p $BUILD_DIR
+TARGET=""
+while getopts "t:" opt; do
+  case $opt in
+    t) TARGET="--target $OPTARG" ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+        exit 1 ;;
+  esac
+done
+
 # cd build/
 cmake -DCMAKE_BUILD_TYPE=Debug -B$BUILD_DIR -H.
-cmake --build $BUILD_DIR -- VERBOSE=2
+cmake --build $BUILD_DIR $TARGET -- VERBOSE=2
